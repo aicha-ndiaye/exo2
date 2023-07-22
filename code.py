af29a8d297e62f79285777509f6fcd66a21642c9
aicha-ndiaye <140071366+aicha-ndiaye@users.noreply.github.com>
2023-07-22 17:07:41 +0100

    Update code.py
    commit change

--- code.py
+++ code.py
@@ -2,3 +2,5 @@ nbr1 = input('Entrez le premier nombre: ')
 nbr2 = input('Entrez le deuxième nombre: ')
 s = int(nbr1) + int(nbr2)
 print('La somme de {0} et {1} est {2}'. format(nbr1, nbr2, s))
+def division(a,b):
+    return a/b
